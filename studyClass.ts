32bfccbf2ac0c1859cee22dbe0e5cf54a92ad4dc
lizhen12345 <1259744231@qq.com>
2020-09-28 15:43:50 +0800

    fixed

--- studyClass.ts
+++ studyClass.ts
@@ -2,18 +2,18 @@
 
 //1,类的基本使用
 
-class Lady {
+class Lady1 {
     content = 'Hello'
     sayHi():string{
         return this.content
     }
 }
 
-const llin = new Lady
+const llin = new Lady1
 
 //2,类的继承，和es6中的继承是一样的
 
-class hhhh extends Lady {
+class hhhh extends Lady1 {
     saylove(){
         return 'i love you'
     }
@@ -24,7 +24,7 @@ class hhhh extends Lady {
 //重写就是子类可以重新编写父类中的代码
 // 如下
 
-class linzhen extends Lady {
+class linzhen extends Lady1 {
     sayHi(){//重写了父类中的sayHi方法
         return 'hhh'
     }
@@ -32,7 +32,7 @@ class linzhen extends Lady {
 
 //3,super关键词的使用，基本和es6相同
 
-class xiaojiejie1 extends Lady{
+class xiaojiejie1 extends Lady1{
     saylove(){//用父类中的方法
         return super.sayHi()+'hh'
     }
